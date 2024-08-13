ebbb6c14cdcdfcad87483e2eca23be8a2fdeeec1
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-08-13 13:47:30 +1200

    Upgrade drizzle

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit"
 
-export default {
+export default defineConfig({
   dialect: 'sqlite',
   schema: './src/db/schema.ts',
   out: './migrations',
   driver: 'expo',
   verbose: false,
   strict: true,
-} satisfies Config
+})
